34a0c2726d23e7934ae46c34cd7664a4524717d5
Crowdbotics <team@crowdbotics.com>
2022-05-13 02:22:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mavrick_delivery_35195",
+  url: "https://mavrick_delivery_35195.botics.co",
+  api: "https://mavrick_delivery_35195.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
